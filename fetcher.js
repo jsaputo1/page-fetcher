176efa177c60f11d079dd89eaeadc86b1d11588c
John Saputo <jsaputo1@gmail.com>
2020-06-04 01:34:45 +0000

    added option for user to overwrite the file

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,9 @@
 const args = process.argv.slice(2);
 const url = args[0];
 const localPath = args[1];
-
 const request = require("request");
 const fs = require("fs");
-
-// const request = require("request");
-// request(test, (error, response, body) => {
-//   console.log; // Print the error if one occurred
-//   // Print the response status code if a response was received
-//   console.log("body:", body); // Print the HTML for the Google homepage.
-// });
+const readline = require("readline");
 
 const fetcher = function (url, localPath) {
   request(url, (error, response, body) => {
@@ -20,23 +13,40 @@ const fetcher = function (url, localPath) {
       return console.log("statusCode:", response && response.statusCode);
     } else {
       fs.writeFile(localPath, body, { flag: "wx" }, function (err) {
-        if (err.code === "ENOENT") {
-          return console.log("Invalid file path", err.path);
-        } else if (err.code === "EEXIST") {
-          return console.log("File already exists", err.path);
+        if (err) {
+          if (err.code === "ENOENT") {
+            return console.log("Invalid file path");
+          } else if (err.code === "EEXIST") {
+            const rl = readline.createInterface({
+              input: process.stdin,
+              output: process.stdout,
+            });
+            rl.question(
+              "File already exists. To overwrite type Y then enter \n",
+              (answer) => {
+                if (answer === "Y") {
+                  path = localPath;
+                  const stat = fs.statSync(path);
+                  const size = stat.size;
+                  console.log(`Downloaded and saved ${size} bytes to ${path}`);
+                  rl.close();
+                } else {
+                  console.log("You did not enter the right key");
+                  rl.close();
+                }
+              }
+            );
+            return;
+          }
         }
-
         path = localPath;
         const stat = fs.statSync(path);
         const size = stat.size;
-
         console.log(`Downloaded and saved ${size} bytes to ${path}`);
+        return;
       });
     }
   });
 };
 
 fetcher(url, localPath);
-
-// What should happen if the local file path given already exists?
-// Optional / Stretch: If the file already exists, let the user know and prompt them to type in Y(followed by the enter key) to overwrite the file, otherwise skip and exit the app. We suggest using the readline module, which we've previously used.
